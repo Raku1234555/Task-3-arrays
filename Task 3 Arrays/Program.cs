1131b4fe0af98b4c4ed0dbab0cbab4306065e15f
Grzegorz Rakowicz <raku1234554321@gmail.com>
2025-01-15 20:39:52 +0100

    Task 5 of 04Arrays exercises

---
+++ Task 3 Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+
+        SieveOfEratosthenes(n);
+    }
+
+    static void SieveOfEratosthenes(int n)
+    {
+
+        bool[] isPrime = new bool[n + 1];
+
+
+        for (int i = 2; i <= n; i++)
+        {
+            isPrime[i] = true;
+        }
+
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+
+        Console.WriteLine("Prime numbers between 1 and " + n + ":");
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime[i])
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
